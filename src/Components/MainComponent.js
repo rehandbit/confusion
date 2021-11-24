36a7d53375d658ed57d1739f656e1fa8c73cc32a
rehandbit <rehan.farazmdp@gmail.com>
2021-11-25 01:33:03 +0530

    SPA part1

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Home from './HomeComponents'
 import Header from './HeaderComponent'
+import Contact from './ContactComponent'
 import Footer from './FooterComponent'
-import Menu from './MenuComponents';
+import Menu from './MenuComponents'
 // import DishDetail from './DishdetailComponent'
-import { DISHES } from '../shared/dishes';
+import { DISHES } from '../shared/dishes'
+import {COMMENTS} from '../shared/comments'
+import {LEADERS} from '../shared/leaders'
+import {PROMOTIONS} from '../shared/promotions'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 class Main extends Component{
@@ -12,26 +16,27 @@ class Main extends Component{
     super(props);
     this.state = {
       dishes : DISHES,
-      selectedDish: null
-    }
+      comments : COMMENTS,
+      promotions : PROMOTIONS,
+      leaders : LEADERS,
+      }
   }
-  onDisheSelect(dishId) {
-   this.setState ({ selectedDish: dishId  })
-}
-
   render() {
     const HomePage = () => {
       return(
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       )
     }
     return (
       <div >
-        
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
 	      {/* <Menu dishes={this.state.dishes}  onClick={(dishId) => this.onDisheSelect(dishId)} />
