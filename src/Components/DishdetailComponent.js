d803eccc6b5d5170f0ca3cea9f667d73d7c428f3
rehandbit <rehan.farazmdp@gmail.com>
2021-12-06 00:33:30 +0530

    Combining Reducers

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -11,7 +11,7 @@ class CommentForm extends Component {
     constructor(props) {
         super(props) 
             this.state = {
-                isNavOpen: false,
+                
                 isModalOpen: false
             }
             this.toggleModal = this.toggleModal.bind(this);
